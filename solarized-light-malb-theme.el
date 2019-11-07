7382c7823f3be9fe7001547287c8e9bf58a2f24a
Martin R. Albrecht <martinralbrecht@googlemail.com>
2019-11-07 17:35:01 +0000

    whitespace

--- solarized-light-malb-theme.el
+++ solarized-light-malb-theme.el
@@ -1,61 +1,61 @@
 (require 'solarized-palettes)
 
-(deftheme solarized-light-malb
-  "my modifications on top of solarized-light")
+(deftheme solarized-light-malb "my modifications on top of solarized-light")
 
-(eval `(solarized-with-color-variables 'light ,solarized-light-color-palette-alist
-         (custom-theme-set-faces
-          `solarized-light-malb
-          ;; steal org style from Leuven
-          `(org-checkbox ((t (:weight bold :box (:line-width 1 :color nil :style (quote pressed-button))
-                                      :foreground "white" :background "light gray"))))
-          `(org-done     ((t (:weight bold :box (:line-width 1 :color "#BBBBBB" :style nil) :foreground "#8BB300" :background "#F0F0F0"))))
-          `(org-scheduled-previously ((t (:foreground ,orange))))
-          `(org-tag ((t (:weight normal :box (:line-width 1 :color "#BBBBBB" :style nil) :foreground "#9A9FA4"))))
-          `(org-todo ((t (:weight bold :box (:line-width 1 :color "#D8ABA7" :style nil) :foreground ,orange :background "#FFE6E4"))))
-          `(org-block-begin-line ((t (:slant normal :foreground ,s-base00 :background ,s-base2 :inherit (org-meta-line)))))
-          `(org-block-end-line   ((t (:slant normal :foreground ,s-base00 :background ,s-base2 :inherit (org-meta-line)))))
-          `(org-block ((t (:foreground ,s-base00 :background ,(solarized-color-blend "#cccccc" s-base3 0.2)))))
+(eval
+ `(solarized-with-color-variables 'light ,solarized-light-color-palette-alist
+    (custom-theme-set-faces
+     `solarized-light-malb
+     ;; steal org style from Leuven
+     `(org-checkbox ((t (:weight bold :box (:line-width 1 :color nil :style (quote pressed-button))
+                                 :foreground "white" :background "light gray"))))
+     `(org-done     ((t (:weight bold :box (:line-width 1 :color "#BBBBBB" :style nil) :foreground "#8BB300" :background "#F0F0F0"))))
+     `(org-scheduled-previously ((t (:foreground ,orange))))
+     `(org-tag ((t (:weight normal :box (:line-width 1 :color "#BBBBBB" :style nil) :foreground "#9A9FA4"))))
+     `(org-todo ((t (:weight bold :box (:line-width 1 :color "#D8ABA7" :style nil) :foreground ,orange :background "#FFE6E4"))))
+     `(org-block-begin-line ((t (:slant normal :foreground ,s-base00 :background ,s-base2 :inherit (org-meta-line)))))
+     `(org-block-end-line   ((t (:slant normal :foreground ,s-base00 :background ,s-base2 :inherit (org-meta-line)))))
+     `(org-block ((t (:foreground ,s-base00 :background ,(solarized-color-blend "#cccccc" s-base3 0.2)))))
 
-          `(markdown-code-face ((t (:background ,(solarized-color-blend "#cccccc" s-base3 0.2) :inherit nil ))))
-          `(markdown-language-keyword-face ((t (:background ,(solarized-color-blend "#cccccc" s-base3 0.2) :inherit (highlight)))))
+     `(markdown-code-face ((t (:background ,(solarized-color-blend "#cccccc" s-base3 0.2) :inherit nil ))))
+     `(markdown-language-keyword-face ((t (:background ,(solarized-color-blend "#cccccc" s-base3 0.2) :inherit (highlight)))))
 
-          `(hl-sentence ((t (:background ,(solarized-color-blend "#cccccc" s-base3 0.2) :inherit (highlight)))))
-          `(which-func  ((t (:foreground ,green :weight bold))))
-          `(stripe-highlight ((t (:background ,s-base2))))
+     `(hl-sentence ((t (:background ,(solarized-color-blend "#cccccc" s-base3 0.2) :inherit (highlight)))))
+     `(which-func  ((t (:foreground ,green :weight bold))))
+     `(stripe-highlight ((t (:background ,s-base2))))
 
-          `(sp-wrap-overlay-opening-pair ((t (:foreground ,green))))
-          `(sp-wrap-overlay-closing-pair ((t (:foreground ,orange))))
+     `(sp-wrap-overlay-opening-pair ((t (:foreground ,green))))
+     `(sp-wrap-overlay-closing-pair ((t (:foreground ,orange))))
 
-          ;; steal spacemacs flycheck style
-          `(flycheck-error   ((t (:underline (:color "#dc322f" :style line)))))
-          `(flycheck-warning ((t (:underline (:color "#b58900" :style line)))))
-          `(flycheck-info    ((t (:underline (:color "#268bd2" :style line)))))
-          `(flycheck-fringe-warning ((t (:weight bold :foreground "#DEB542" :background ,s-base3))))
-          `(flycheck-fringe-error   ((t (:weight bold :foreground "#dc322f" :background ,s-base3))))
-          `(flycheck-fringe-info    ((t (:weight bold :foreground "#69B7F0" :background ,s-base3))))
+     ;; steal spacemacs flycheck style
+     `(flycheck-error   ((t (:underline (:color "#dc322f" :style line)))))
+     `(flycheck-warning ((t (:underline (:color "#b58900" :style line)))))
+     `(flycheck-info    ((t (:underline (:color "#268bd2" :style line)))))
+     `(flycheck-fringe-warning ((t (:weight bold :foreground "#DEB542" :background ,s-base3))))
+     `(flycheck-fringe-error   ((t (:weight bold :foreground "#dc322f" :background ,s-base3))))
+     `(flycheck-fringe-info    ((t (:weight bold :foreground "#69B7F0" :background ,s-base3))))
 
-          `(eshell-git-prompt-powerline-dir-face ((t (:foreground ,s-base3 :background ,blue))))
-          `(eshell-git-prompt-powerline-clean-face ((t (:foreground ,s-base3 :background ,green))))
-          `(eshell-git-prompt-powerline-not-clean-face ((t (:foreground ,s-base3 :background ,orange))))
+     `(eshell-git-prompt-powerline-dir-face ((t (:foreground ,s-base3 :background ,blue))))
+     `(eshell-git-prompt-powerline-clean-face ((t (:foreground ,s-base3 :background ,green))))
+     `(eshell-git-prompt-powerline-not-clean-face ((t (:foreground ,s-base3 :background ,orange))))
 
-          `(dired-subtree-depth-1-face ((t (:background ,(solarized-color-blend "#cccccc" s-base3 0.2)))))
-          `(dired-subtree-depth-2-face ((t (:background ,(solarized-color-blend "#cccccc" s-base3 0.2)))))
-          `(dired-subtree-depth-3-face ((t (:background ,(solarized-color-blend "#cccccc" s-base3 0.2)))))
-          `(dired-subtree-depth-4-face ((t (:background ,(solarized-color-blend "#cccccc" s-base3 0.2)))))
-          `(dired-subtree-depth-5-face ((t (:background ,(solarized-color-blend "#cccccc" s-base3 0.2)))))
-          `(dired-subtree-depth-6-face ((t (:background ,(solarized-color-blend "#cccccc" s-base3 0.2)))))
+     `(dired-subtree-depth-1-face ((t (:background ,(solarized-color-blend "#cccccc" s-base3 0.2)))))
+     `(dired-subtree-depth-2-face ((t (:background ,(solarized-color-blend "#cccccc" s-base3 0.2)))))
+     `(dired-subtree-depth-3-face ((t (:background ,(solarized-color-blend "#cccccc" s-base3 0.2)))))
+     `(dired-subtree-depth-4-face ((t (:background ,(solarized-color-blend "#cccccc" s-base3 0.2)))))
+     `(dired-subtree-depth-5-face ((t (:background ,(solarized-color-blend "#cccccc" s-base3 0.2)))))
+     `(dired-subtree-depth-6-face ((t (:background ,(solarized-color-blend "#cccccc" s-base3 0.2)))))
 
-          `(ein:cell-input-area   ((t (:background ,(solarized-color-blend "#cccccc" s-base3 0.15)))))
-          `(ein:cell-input-prompt ((t (:weight bold :foreground ,s-base00 :inherit (header-line)))))
+     `(ein:cell-input-area   ((t (:background ,(solarized-color-blend "#cccccc" s-base3 0.15)))))
+     `(ein:cell-input-prompt ((t (:weight bold :foreground ,s-base00 :inherit (header-line)))))
 
-          `(bm-face ((t (:background "#FFE6E4"))))
-          `(helm-locate-finish ((t (:foreground ,green :weight bold))))
-          `(slack-message-output-header ((t (:weight bold))))
-          `(slack-message-output-text ((t (:weight normal))))
-          `(slack-new-message-marker-face ((t (:foreground "#d33682" :weight bold :height 0.75))))
-          `(helm-xref-file-name ((t (:foreground ,s-base0))))
-          )))
+     `(bm-face ((t (:background "#FFE6E4"))))
+     `(helm-locate-finish ((t (:foreground ,green :weight bold))))
+     `(slack-message-output-header ((t (:weight bold))))
+     `(slack-message-output-text ((t (:weight normal))))
+     `(slack-new-message-marker-face ((t (:foreground "#d33682" :weight bold :height 0.75))))
+     `(helm-xref-file-name ((t (:foreground ,s-base0))))
+     )))
 
 (eval `(solarized-with-color-variables 'light ,solarized-light-high-contrast-palette-alist
          (custom-theme-set-variables
