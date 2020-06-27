740d7eafb42f8b2f2ce5760a91533ce18375630a
Martin R. Albrecht <martinralbrecht@googlemail.com>
2020-06-27 20:29:52 +0100

    helm-baloo tweaks

--- local/helm-baloo.el
+++ local/helm-baloo.el
@@ -5,6 +5,7 @@
 
 (require 'helm-source)
 (require 'helm-locate)
+(require 'helm-files)
 
 (defcustom helm-baloo-file-limit 100
   "Limit number of entries returned by baloo to this number."
@@ -40,27 +41,45 @@
     ("Find file as root"                     . helm-find-file-as-root)
     ("Find file other window"                . helm-find-files-other-window)
     ("Find file other frame"                 . find-file-other-frame)
-    ("Open Dired in file's directory"        . helm-open-dired)
-    ("Grep File(s) `C-u recurse'"            . helm-find-files-grep)
-    ("Zgrep File(s) `C-u Recurse'"           . helm-ff-zgrep)
-    ("Pdfgrep File(s)"                       . helm-ff-pdfgrep)
+    ("Open Dired in file's directory `C-d'"  . helm-open-dired)
     ("Insert as org link"                    . helm-files-insert-as-org-link)
     ("Checksum File"                         . helm-ff-checksum)
     ("Ediff File"                            . helm-find-files-ediff-files)
-    ("Ediff Merge File"                      . helm-find-files-ediff-merge-files)
-    ("Etags `M-., C-u reload tag file'"      . helm-ff-etags-select)
     ("View file"                             . view-file)
     ("Insert file"                           . insert-file)
-    ("Add marked files to file-cache"        . helm-ff-cache-add-file)
-    ("Delete file(s)"                        . helm-delete-marked-files)
-    ("Copy file(s) `M-C, C-u to follow'"     . helm-find-files-copy)
-    ("Rename file(s) `M-R, C-u to follow'"   . helm-find-files-rename)
-    ("Symlink files(s) `M-S, C-u to follow'" . helm-find-files-symlink)
-    ("Relsymlink file(s) `C-u to follow'"    . helm-find-files-relsymlink)
-    ("Hardlink file(s) `M-H, C-u to follow'" . helm-find-files-hardlink)
     ("Open file externally (C-u to choose)"  . helm-open-file-externally)
     ("Open file with default tool"           . helm-open-file-with-default-tool)
-    ("Find file in hex dump"                 . hexl-find-file)))
+    ("Copy file(s) `M-C, C-u' to follow"     . helm-find-files-copy)
+    ("Rename file(s) `M-R, C-u' to follow"   . helm-find-files-rename)
+    ("Symlink files(s) `M-S, C-u' to follow" . helm-find-files-symlink)))
+
+(defvar helm-baloo-map
+  (let ((map (make-sparse-keymap)))
+    (set-keymap-parent map helm-map)
+    (define-key map (kbd "C-]")     'helm-ff-run-toggle-basename)
+    (define-key map (kbd "M-R")     'helm-ff-run-rename-file)
+    (define-key map (kbd "M-C")     'helm-ff-run-copy-file)
+    (define-key map (kbd "M-B")     'helm-ff-run-byte-compile-file)
+    (define-key map (kbd "M-L")     'helm-ff-run-load-file)
+    (define-key map (kbd "M-S")     'helm-ff-run-symlink-file)
+    (define-key map (kbd "M-H")     'helm-ff-run-hardlink-file)
+    (define-key map (kbd "M-D")     'helm-ff-run-delete-file)
+    (define-key map (kbd "C-=")     'helm-ff-run-ediff-file)
+    (define-key map (kbd "C-c o")   'helm-ff-run-switch-other-window)
+    (define-key map (kbd "C-c r")   'helm-ff-run-find-file-as-root)
+    (define-key map (kbd "C-c C-o") 'helm-ff-run-switch-other-frame)
+    (define-key map (kbd "M-i")     'helm-ff-properties-persistent)
+    (define-key map (kbd "C-c C-x") 'helm-ff-run-open-file-externally)
+    (define-key map (kbd "C-c X")   'helm-ff-run-open-file-with-default-tool)
+    (define-key map (kbd "C-c @")   'helm-ff-run-insert-org-link)
+    (define-key map (kbd "C-d")     (lambda ()
+                                      (interactive)
+                                      (with-helm-alive-p
+                                        (helm-exit-and-execute-action
+                                         'helm-open-dired))))
+    (define-key map (kbd "C-c C-a") 'malb/helm-mml-attach-files)
+    map)
+  "Keymap for Helm Baloo.")
 
 (defun helm-baloo-no-directory ()
   (interactive)
@@ -68,7 +87,7 @@
                    :candidates-process #'helm-baloo-search
                    :candidate-transformer '(helm-baloo-transform helm-skip-boring-files)
                    :action helm-baloo-actions
-                   :keymap helm-generic-files-map
+                   :keymap helm-baloo-map
                    :help-message #'helm-generic-file-help-message)
         :buffer "*helm baloo*"))
 
@@ -78,7 +97,7 @@
                    :candidates-process (lambda () (helm-baloo-search directory))
                    :candidate-transformer '(helm-baloo-transform helm-skip-boring-files)
                    :action helm-baloo-actions
-                   :keymap helm-generic-files-map
+                   :keymap helm-baloo-map
                    :help-message #'helm-generic-file-help-message)
         :buffer "*helm baloo*"))
 
