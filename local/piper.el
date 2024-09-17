58ad7b11f5b99871427b7f4b0de6cb1faa9066f0
Martin R. Albrecht <martinralbrecht@googlemail.com>
2024-09-17 10:14:47 +0100

    bugfix

--- local/piper.el
+++ local/piper.el
@@ -26,7 +26,7 @@
 These are tuples of (\"tag\" \"lang-code\" \"model-path\" \"speaker-number\")."
 
   :group 'piper
-  :type '(alist :value-type (string string string string)))
+  :type '(alist :value-type (string string string integer)))
 
 (defcustom piper-command "cat ${input-filename} | piper -m ${voice} -s ${speaker-number} -f ${output-filename}"
   "Command to run Piper."
